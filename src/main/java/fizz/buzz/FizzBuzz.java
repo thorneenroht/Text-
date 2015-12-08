07e90860672cff18e5af85b5ac9cf914189f2b00
Matthew Thorne <matthew.thorne.s@gmail.com>
2015-12-07 23:50:07 -0500

    Removing unused import

--- src/main/java/fizz/buzz/FizzBuzz.java
+++ src/main/java/fizz/buzz/FizzBuzz.java
@@ -1,7 +1,5 @@
 package fizz.buzz;
 
-import javax.swing.plaf.synth.SynthSeparatorUI;
-
 /*Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead 
  * of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both 
  * three and five print “FizzBuzz”.*/
